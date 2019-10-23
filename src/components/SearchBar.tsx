c72d5e7318d53f0b0f966e11b0a02f2d10d4afa7
soohyun0131.lee <soohyun0131.lee@lge.com>
2019-10-23 16:24:59 +0900

    rename function

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
-export default function BackupCustomizedInputBase() {
+export default function CustomizedInputBase() {
   const classes = useStyles();
 
   return (
