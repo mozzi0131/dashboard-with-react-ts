8c4f674653b936568c52addb1ec455cb8c8489f6
soohyun0131.lee <soohyun0131.lee@lge.com>
2019-10-24 09:10:55 +0900

    refactoring(?) codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,173 @@
 import React, { useState, useEffect } from 'react';
 
-import TestDataTable from './components/DataTable';
-import SearchBar from "./components/SearchBar";
-import { totalCol, unittestCol, coverageCol, setColumnData, totalRows, unittestRows, coverageRows } from './typescripts/manageTableData'
+import TestDataTable, { Column } from './components/DataTable';
+import SearchBar from './components/SearchBar';
+import ProgressBar from './components/ProgressBar';
+import { TestResult, getTestResults } from './utils/jenkins';
 
-import ProgressBar from "./components/ProgressBar"
+// TODO 각 column의 format 함수 구현해서 빨간줄 없애기
+export const totalCol: Column[] = [
+  {
+    id: 'module',
+    label: 'Module',
+    minWidth: 100,
+    format: testResult => testResult.moduleName,
+  },
+  { id: 'passRate', label: 'Pass\u00a0Rate', minWidth: 100, align: 'right' },
+  {
+    id: 'utNum',
+    label: '#\u00a0UT',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'failNum',
+    label: '#\u00a0F',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'coveredLine',
+    label: '#\u00a0LN',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'linePercentage',
+    label: '%\u00a0LN',
+    minWidth: 100,
+    align: 'right',
+  },
+  {
+    id: 'validLine',
+    label: 'VALID\u00a0LN',
+    minWidth: 150,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+];
+
+export const unittestCol: Column[] = [
+  { id: 'module', label: 'Module', minWidth: 100 },
+  { id: 'passRate', label: 'Pass\u00a0Rate', minWidth: 100, align: 'right' },
+  {
+    id: 'utNum',
+    label: '#\u00a0UT',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'failNum',
+    label: '#\u00a0F',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'errNum',
+    label: 'Error',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'memoryLeak',
+    label: '# LEAK',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+];
+
+export const coverageCol: Column[] = [
+  { id: 'module', label: 'Module', minWidth: 100 },
+  {
+    id: 'coveredLine',
+    label: '#\u00a0LN',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'linePercentage',
+    label: '%\u00a0LN',
+    minWidth: 100,
+    align: 'right',
+  },
+  {
+    id: 'validLine',
+    label: 'VALID\u00a0LN',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'coveredBranch',
+    label: '#\u00a0BR',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'branchPercentage',
+    label: '%\u00a0BR',
+    minWidth: 100,
+    align: 'right',
+  },
+  {
+    id: 'validBranch',
+    label: 'VALID\u00a0BR',
+    minWidth: 100,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+];
 
 const App: React.FC = () => {
-    const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [testResults, setTestResults] = useState<TestResult[]>([]);
 
-    useEffect(() => {
-        async function setModuleData() {
-            await setColumnData();
-            setLoading(true);
-        }
-        setModuleData();
-    }, []);
+  useEffect(() => {
+    (async function() {
+      setLoading(true);
+      try {
+        const fetchedTestResults = await getTestResults();
+        setTestResults(fetchedTestResults);
+      } catch (err) {
+        // TODO 에러 메시지 표시?
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, []);
 
-    return (
+  return (
+    <div>
+      {loading ? (
         <div>
-            {loading === false && <div> <ProgressBar/> </div> }
-            {loading === true &&
-                <div className="App">
-                    <div id="mainbody">
-                        <h1>Dashboard - Unit test report</h1>
-                        <SearchBar />
-                        <h2>Total Info</h2>
-                        <TestDataTable columns={totalCol} rows={totalRows} />
-                        <h2>Unit Test</h2>
-                        <TestDataTable columns={unittestCol} rows={unittestRows} />
-                        <h2>Code Coverage</h2>
-                        <TestDataTable columns={coverageCol} rows={coverageRows} />
-                    </div>
-                </div>
-            }
-
+          <ProgressBar />
         </div>
-
-    );
-}
+      ) : (
+        <div className="App">
+          <div id="mainbody">
+            <h1>Dashboard - Unit test report</h1>
+            <SearchBar />
+            <h2>Total Info</h2>
+            <TestDataTable columns={totalCol} rows={testResults} />
+            <h2>Unit Test</h2>
+            <TestDataTable columns={unittestCol} rows={testResults} />
+            <h2>Code Coverage</h2>
+            <TestDataTable columns={coverageCol} rows={testResults} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -7,83 +7,107 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import { TestResult } from '../utils/jenkins';
+
+export interface Column {
+  id: string;
+  label: string;
+  minWidth?: number;
+  align?: 'right' | 'center';
+  format: (testResult: TestResult) => string;
+  link?: boolean;
+}
 
 const useStyles = makeStyles({
-    root: {
-        width: '100%',
-    },
-    tableWrapper: {
-        maxHeight: 440,
-        overflow: 'auto',
-    },
+  root: {
+    width: '100%',
+  },
+  tableWrapper: {
+    maxHeight: 440,
+    overflow: 'auto',
+  },
 });
 
 //이 함수의 columns, rows가 각각 (totalColumn, totalRows)/(unittestColumn, unittestRows)/(coverageColumn, coverageRows)로 props로 보내서 그릴 수 있는..? 방법이 있는지..?
-export default function DataTable({ columns, rows }: any) {
-    const classes = useStyles();
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(2);
+export default function DataTable({
+  columns,
+  rows,
+}: {
+  columns: Column[];
+  rows: TestResult[];
+}) {
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(2);
 
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
 
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-    return (
-        <Paper className={classes.root}>
-            <div className={classes.tableWrapper}>
-                <Table stickyHeader>
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column: any) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{ minWidth: column.minWidth }}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row: any) => {
-                                return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.module}>
-                                        {columns.map((column: any) => {
-                                            const value = row[column.id];
-                                            return (
-                                                <TableCell key={column.id} align={column.align}>
-                                                    {column.format && typeof value === 'number' ? column.format(value) : value}
-                                                </TableCell>
-                                            );
-                                        })}
-                                    </TableRow>
-                                );
-                            })}
-                    </TableBody>
-                </Table>
-            </div>
-            <TablePagination
-                rowsPerPageOptions={[1, 2, 4]}
-                component="div"
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                backIconButtonProps={{
-                    'aria-label': 'previous page',
-                }}
-                nextIconButtonProps={{
-                    'aria-label': 'next page',
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-        </Paper>
-    );
+  return (
+    <Paper className={classes.root}>
+      <div className={classes.tableWrapper}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map(row => {
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.moduleName}
+                  >
+                    {columns.map(column => {
+                      const value = column.format(row);
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[1, 2, 4]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'previous page',
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'next page',
+        }}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
 }
